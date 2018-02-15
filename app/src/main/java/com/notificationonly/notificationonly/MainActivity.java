d5d5090a43e1786af83bcfe336d512447d4cf5f8
ts-3156 <ts_3156@yahoo.co.jp>
2018-02-16 01:38:13 +0900

    Implement send_notification

--- app/src/main/java/com/notificationonly/notificationonly/MainActivity.java
+++ app/src/main/java/com/notificationonly/notificationonly/MainActivity.java
@@ -1,13 +1,69 @@
 package com.notificationonly.notificationonly;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        count = 0;
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendNotification(MainActivity.this, "Title", "Text " + count++);
+            }
+        });
     }
+
+    void sendNotification(Context context, String title, String text) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
+
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        String channelId = context.getString(R.string.channel_id);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(context, channelId)
+                        .setSmallIcon(R.drawable.ic_notif)
+                        .setContentTitle(title)
+                        .setContentText(text)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent)
+                        .setTicker(text);
+
+        NotificationManager manager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (manager.getNotificationChannel(channelId) == null) {
+                String channelName = context.getString(R.string.channel_name);
+                NotificationChannel channel =
+                        new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+                manager.createNotificationChannel(channel);
+            }
+        }
+
+        int notificationId = 1;
+        manager.notify(notificationId, builder.build());
+    }
+
 }
